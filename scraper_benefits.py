a760830c57612cf8cbb1e99f1f1516e4f7a62c86
mouh2020 <badimohammed2019@gmail.com>
2023-07-13 01:08:34 +0100

    add article pages number

--- scraper_benefits.py
+++ scraper_benefits.py
@@ -12,41 +12,37 @@ create_db_and_tables()
 recipes_url ="https://www.justfood.tv/الفئة/معجنات/بيتزا/next_page_number/articles_page_number"
 
 for i in range(1,500) : 
-    time.sleep(1.5)
     try :
-        for j in range(1,500) :
-            url_to_scrape = build_decoding_url(url=recipes_url,
-                                            next_page_number=str(i),
-                                            articles_page_number=str(j))
-            logger.info(f'url_to_scrape : {url_to_scrape}')
-            page_html_code = make_request(url=url_to_scrape)
-            if page_html_code == None : 
-                continue
+        time.sleep(1.5)
+        url_to_scrape = build_decoding_url(url=recipes_url,
+                                                    next_page_number=str(i),
+                                                    articles_page_number="1")
+        logger.info(f'scrap first page url : {url_to_scrape}')
+        page_html_code = make_request(url=url_to_scrape)
+        if page_html_code == None : 
+            continue
+        article_pages_number = get_article_pages_number(page_html_code)
+        logger.info(f'article pages number : {str(article_pages_number)}')
+        if article_pages_number == None : 
+            continue
+        for j in range(1,article_pages_number+1) : 
             articles_links = get_all_articles_links(html_code=page_html_code)
-            if articles_links == None or len(articles_links) < 7: 
-                break
             logger.info(f'articles_links_number : {len(articles_links)}')
+            if articles_links == None :  
+                continue
             for article_link in articles_links : 
-                ##### Process article.
                 article_id = int(get_article_id(link=article_link))
                 database_session = Session(engine)
                 fetched_article =database_session.exec(select(Article).where(Article.article_id==article_id)).first()
                 if fetched_article : 
-                    logger.info(f'found duplicate article : {str(article_id)}')
                     continue
                 logger.info(f'fetch article_id  : {str(article_id)}')
                 article_html_code = make_request(url=article_link)
-                #print(article_html_code)
                 article_body_html = get_article_body(article_html_code=article_html_code)
                 article = clean_article(str(article_body_html))
-                #### Add images
-                #article = add_image_link_to_article(article)
-                ##### Process article title.
                 title = get_title(article_html_code=article_html_code)
                 title = clean_title(title=title)
-                #### Check if article exists.
-                #### Insert article.
-                logger.info(f'article title  : {str(article_id)}')
+                logger.info(f'article title  : {str(title)}')
                 article_to_insert = Article(
                     article_id=article_id,
                     title=title,
@@ -55,7 +51,12 @@ for i in range(1,500) :
                 )
                 database_session.add(article_to_insert)
                 database_session.commit()
+                database_session.close()
                 time.sleep(1.5)
+                url_to_scrape = build_decoding_url(url=recipes_url,
+                                                next_page_number=str(i),
+                                                articles_page_number=str(j))
+                page_html_code = make_request(url=url_to_scrape)
     except Exception as e : 
         logger.error(f'error occured : {str(e)}')
 
